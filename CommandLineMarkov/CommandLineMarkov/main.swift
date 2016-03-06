434d8b8ae78e3c422ace4ffeccf6f01e9848bad6
Adam Boyd <adamboyd50@gmail.com>
2016-03-05 19:09:45 -0800

    blank initial project

---
+++ CommandLineMarkov/CommandLineMarkov/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CommandLineMarkov
+//
+//  Created by Adam Boyd on 2016-03-05.
+//  Copyright © 2016 Adam. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
